f4cadf44d62daaf94a6aaedede4d741a606a19e7
Vladimir Tsepelev <tsepelev@openmailbox.org>
2017-11-09 00:43:32 +0300

    currently under development

--- polynomial.py
+++ polynomial.py
@@ -8,17 +8,19 @@ class Polynomial:
         self.n = 0
         self.visual = ""
     def degree(self):
-        self.n = len(coefficients)
+        self.n = len(self.coefficients)
         return int(self.n)
     def output(self):
-        for i in range(self.n):
-            self.visual = self.visual + str(coefficients[i]) + "t^" + str(i)
+        self.visual = str(self.coefficients[0])
+        for i in range(1,self.n-1):
+            self.visual = self.visual + "+" + str(self.coefficients[i]) + "t^" + str(i)
+        self.visual = self.visual + "+" + str(self.coefficients[self.n-1]) + "t^" + str(self.n-1)
         return str(self.visual)
 
-arr = [1,2,3]
+arr = [1,2,3,4,5]
 obj = Polynomial(arr)
-n = obj.degree
-out = obj.output
+n = obj.degree()
+out = obj.output()
 print(n)
 print(out)
         
