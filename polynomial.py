7abcb6660939b528ea7ca7d4fadb262f49078ee7
Vladimir Tsepelev <tsepelev@openmailbox.org>
2017-11-09 01:17:48 +0300

    currently under development

--- polynomial.py
+++ polynomial.py
@@ -1,6 +1,4 @@
-# This class is currently under construction.
-
-import math
+# This class is currently under development.
 
 class Polynomial:
     def __init__(self, coefficients):
@@ -9,18 +7,24 @@ class Polynomial:
         self.visual = ""
     def degree(self):
         self.n = len(self.coefficients)
-        return int(self.n)
+        return int(self.n)        
+    def __add__(self, other):
+        for i in range(max(self.degree(), other.degree())):
+            self.coefficients[i] = self.coefficients[i] + other.coefficients[i]
+        new_coefficients = self.coefficients
+        return Polynomial(new_coefficients)
     def output(self):
         self.visual = str(self.coefficients[0])
-        for i in range(1,self.n-1):
+        for i in range(1, self.n-1):
             self.visual = self.visual + "+" + str(self.coefficients[i]) + "t^" + str(i)
         self.visual = self.visual + "+" + str(self.coefficients[self.n-1]) + "t^" + str(self.n-1)
         return str(self.visual)
 
-arr = [1,2,3,4,5]
-obj = Polynomial(arr)
-n = obj.degree()
-out = obj.output()
-print(n)
-print(out)
+arr1 = [1,2,3,4,5]
+obj1 = Polynomial(arr1)
+arr2 = [6,7,8,9,10]
+obj2 = Polynomial(arr2)
+print((obj1+obj2).output())
+print(obj1.output())
+print(obj2.output())
         
