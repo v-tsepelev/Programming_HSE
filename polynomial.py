a1c5ba923b678e96469034bcb8517e468342c91c
Vladimir Tsepelev <tsepelev@openmailbox.org>
2017-11-24 01:24:39 +0300

    module for work with polynomials

--- polynomial.py
+++ polynomial.py
@@ -1,5 +1,8 @@
 # This class is currently under development.
 
+import math
+import cmath
+
 class Polynomial:
 
     def __init__(self, coefficients):
@@ -16,6 +19,7 @@ class Polynomial:
             for i in powers:
                 self.coefficients.append(coefficients[i])
         self.n = len(self.coefficients)
+        self.count = 0
         self.visual = ""
 
     def degree(self):
@@ -28,8 +32,6 @@ class Polynomial:
                 new_coefficients.append(self.coefficients[i] + other.coefficients[i])
             return Polynomial(new_coefficients)
         else:
-            #new_coefficients = self.coefficients
-            #new_coefficients[0] = self.coefficients[0] + int(other)
             new_coefficients = []
             for i in range(self.degree() + 1):
                 new_coefficients.append(self.coefficients[i])
@@ -106,12 +108,6 @@ class Polynomial:
         return result
     
     def der(self, d = 1):
-        #while d != 1:
-            #new_coefficients = []
-            #for i in range(self.n-1):
-                #new_coefficients.append(self.coefficients[i+1]*(i+1))
-            #d -= 1
-            #return der(Polynomial(new_coefficients), d)
         if d == 1:
             new_coefficients = []
             for i in range(self.degree()):
@@ -132,6 +128,20 @@ class Polynomial:
         
     def dersubst(self, x, d = 1):
         return self.der(d).subst(x)
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.count == 0:
+            self.count += 1
+            return (0, self.coefficients[0])
+        else:
+            if self.count < self.degree() + 1:
+                self.count += 1
+                return (self.count - 1, self.coefficients[self.count - 1])
+            else:
+                raise StopIteration
             
 class RealPolynomial(Polynomial):
     
@@ -157,17 +167,67 @@ class RealPolynomial(Polynomial):
         x = self.der().find_root(left_x, right_x, epsilon = 0.01)
         if self.substder(x, 2) < 0:
             return x
+        
+class QuadraticPolynomial(Polynomial):
+    
+    def __init__(self, coefficients):
+        Polynomial.__init__(self, coefficients)
+        if self.degree() > 2:
+            raise Exception("Вы пытаетесь создать многочлен степени, большей чем 2")
 
-arr1 = [1]
-obj1 = Polynomial(arr1)
-arr2 = [2,3]
-obj2 = Polynomial(arr2)
-arr3 = [4,5,6]
-obj3 = Polynomial(arr3)
-arr4 = [7,8,9,10]
-obj4 = Polynomial(arr4)
-print(obj1)
-print(obj2)
-print(obj3)
-print(obj4)
+    def __mul__(self, other):
+        if isinstance(other, Polynomial):
+            new_coefficients = []
+            for k in range(self.degree() + other.degree() + 1):
+                x = []
+                for i in range(self.degree() + 1):
+                    for j in range(other.degree() + 1):
+                        if (i + j == k):
+                            x.append(self.coefficients[i]*other.coefficients[j])
+                new_coefficients.append(sum(x))
+            result = Polynomial(new_coefficients)
+            if result.degree() > 2:
+                raise DegreeIsTooBig(result.degree())
+            else:
+                return result
+        else:
+            new_coefficients = []
+            for i in range(self.degree() + 1):
+                new_coefficients.append(other*self.coefficients[i])
+            return Polynomial(new_coefficients)
+
+    def solve(self):
+        if self.degree() == 0:
+            return []
+        elif self.degree() == 1:
+            return [-self.coefficients[0]/self.coefficients[1]]
+        else:
+            a = self.coefficients[2]
+            b = self.coefficients[1]
+            c = self.coefficients[0]
+            D = b**2-4*a*c
+            if abs(D) <= 1e-10:
+                x = -b/(2*a)
+                return [x]
+            elif D > 1e-10:
+                x1 = (-b+D**0.5)/(2*a)
+                x2 = (-b-D**0.5)(2*a)
+                return [x1, x2]
+            elif D < -1e-10:
+                c1 = (-b+cmath.sqrt(D))/(2*a)
+                c2 = (-b-cmath.sqrt(D))/(2*a)
+                return [c1, c2]
+        
+class DegreeIsTooBig(Exception):
+    
+    def __init__(self, x, y = 2):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        if isinstance(self.x, str):
+            return self.x
+        elif isinstance(self.x, int):
+            error = "В результате операции получился многочлен степени {0}, максимально допустимая степень {1}".format(self.x, self.y)
+            return error
 
