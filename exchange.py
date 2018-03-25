52343263ba3cfd300caa3f0e8e1e909fb3edab6a
Vladimir Tsepelev <tsepelev@openmailbox.org>
2018-03-25 21:16:54 +0300

    minimum number of coins needed for exchange

---
+++ exchange.py
@@ -0,0 +1,18 @@
+coins = input().split(" ")
+for j in range(len(coins)):
+    coins[j] = int(coins[j])
+n = int(input())
+
+num = []
+num.append(0)
+k = len(coins)
+
+for p in range(1, n + 1):
+    minimum = 1787
+    for i in range(1, k):
+        if coins[i] <= p:
+            if (1 + num[p - coins[i]]) < minimum:
+                minimum = 1 + num[p - coins[i]]
+    num.append(minimum)
+
+print(num[n])
